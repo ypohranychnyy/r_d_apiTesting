0ddb5901c4b8513c4dda62830846d577cdc54893
Yuriy <the.whu@gmail.com>
2024-10-24 20:50:08 +0300

    Додано .gitignore та видалено node_modules з індексу

--- node_modules/@badeball/cypress-cucumber-preprocessor/patches/patch.js
+++
@@ -1,9 +0,0 @@
-/**
- * Even the simplest shell script fails in Windows, hence this file.
- *
- * The intention here is to only run `patch-package` during local installation, IE. during
- * development of the library and not upon installation by the users of the library.
- */
-if (process.cwd() === process.env.INIT_CWD) {
-  require("patch-package");
-}
